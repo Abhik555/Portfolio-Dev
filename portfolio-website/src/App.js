2ca1c44cd01e9c87c4f4d7afddbd6f107e470d9e
Abhik555 <ghoshabhik555@gmail.com>
2023-09-21 21:10:47 +0530

    Website 1

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -4,39 +4,50 @@ import Home from "./pages/home";
 import Projects from "./pages/projects";
 import About from "./pages/about";
 import NoPage from "./pages/nopage";
+import logo from "./resources/logo.png";
 
 function App() {
+  document.body.classList.add("backdrp");
   return (
-    <div>
-      <img src="" alt="backimg"></img>
-      <header>
-        <nav>
-          <BrowserRouter>
-            <h1 className="title">Abhik Ghosh</h1>
-            <div className="navbar">
-              <ul className="list">
-                <li className="item">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="item">
-                  <Link to="/Projects">Projects</Link>
-                </li>
-                <li className="item">
-                  <Link to="/About">About</Link>
-                </li>
-              </ul>
-            </div>
+    <section className="backdrp">
+      <div>
+        <header>
+          <nav>
+            <BrowserRouter>
+              <div className="navbar">
+                <div className="imgwrapper">
+                  <img className="logo" src={logo} alt="logo"></img>
+                </div>
+                <ul className="list">
+                  <li className="item">
+                    <Link className="link" to="/">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="item">
+                    <Link className="link" to="/Projects">
+                      Projects
+                    </Link>
+                  </li>
+                  <li className="item">
+                    <Link className="link" to="/About">
+                      About
+                    </Link>
+                  </li>
+                </ul>
+              </div>
 
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/Projects" element={<Projects />} />
-              <Route path="/About" element={<About />} />
-              <Route path="*" element={<NoPage />} />
-            </Routes>
-          </BrowserRouter>
-        </nav>
-      </header>
-    </div>
+              <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/Projects" element={<Projects />} />
+                <Route path="/About" element={<About />} />
+                <Route path="*" element={<NoPage />} />
+              </Routes>
+            </BrowserRouter>
+          </nav>
+        </header>
+      </div>
+    </section>
   );
 }
 
