aec4c6d8de959fec174ca2e21cc8e771fbaa729b
Abhik555 <ghoshabhik555@gmail.com>
2023-09-21 20:13:40 +0530

    Resource Upload

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Projects from "./pages/projects";
+import About from "./pages/about";
+import NoPage from "./pages/nopage";
 
 function App() {
   return (
     <div>
-      <h1>Test</h1>
+      <img src="" alt="backimg"></img>
+      <header>
+        <nav>
+          <BrowserRouter>
+            <h1 className="title">Abhik Ghosh</h1>
+            <div className="navbar">
+              <ul className="list">
+                <li className="item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="item">
+                  <Link to="/Projects">Projects</Link>
+                </li>
+                <li className="item">
+                  <Link to="/About">About</Link>
+                </li>
+              </ul>
+            </div>
+
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/Projects" element={<Projects />} />
+              <Route path="/About" element={<About />} />
+              <Route path="*" element={<NoPage />} />
+            </Routes>
+          </BrowserRouter>
+        </nav>
+      </header>
     </div>
   );
 }
---
+++ portfolio-website/src/pages/nopage.js
@@ -0,0 +1,10 @@
+function NoPage() {
+    return(
+        <div>
+            <img src="" alt=""></img>
+            <h1>404 Not Found</h1>
+        </div>
+    );
+}
+
+export default NoPage;
