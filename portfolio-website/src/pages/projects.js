72a02122ffa577478380fbc5cb9e0fd51d568096
Abthedev <42204991+Abhik555@users.noreply.github.com>
2023-09-23 23:40:54 +0530

    Project Page Creation

--- portfolio-website/src/pages/projects.js
+++ portfolio-website/src/pages/projects.js
@@ -1,18 +1,19 @@
 import styles from "./styles/projects.module.css";
-import ProjectCard from "../components/projectscard";
-import testimg from ".././resources/me.jpg";
 import Footer from "../components/footer";
 
 function Projects() {
   return (
     <div className={styles.maincontainer}>
-      <h1>Projects</h1>
-      <ProjectCard
-        img={testimg}
-        text="This is test para"
-        title="this is a test title"
-      />
-      <div style={{alignContent:"center" , justifyContent:"center" , display:"flex"}}>
+      <div className={styles.content}>
+        <h1 className={styles.pagetitle}>Projects</h1>
+      </div>
+      <div
+        style={{
+          alignContent: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
         <Footer />
       </div>
     </div>
